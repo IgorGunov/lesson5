5c4900b8b60276403245547957aefdaae4336336
igorgunov1564 <igorgunov1564@yandex.ru>
2023-05-19 11:42:10 +0300

    second commit

--- app/src/main/java/ru/mirea/gunovigor/lesson5/MainActivity.java
+++ app/src/main/java/ru/mirea/gunovigor/lesson5/MainActivity.java
@@ -1,23 +1,21 @@
 package ru.mirea.gunovigor.lesson5;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-import ru.mirea.gunovigor.accelerometer.databinding.ActivityMainBinding;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+import ru.mirea.gunovigor.lesson5.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-
-    private SensorManager sensorManager;
-    private Sensor accelerometerSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,41 +23,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-//        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-//        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        sensorManager.registerListener(this, accelerometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, accelerometerSensor,
-                SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float valueAzimuth = event.values[0];
-            float valuePitch = event.values[1];
-            float valueRoll = event.values[2];
-            binding.textViewAzimuth.setText("Azimuth: " + valueAzimuth);
-            binding.textViewPitch.setText("Pitch: " + valuePitch);
-            binding.textViewRoll.setText("Roll: " + valueRoll);
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        ListView listSensor = binding.sensorListView;
+// создаем список для отображения в ListView найденных датчиков
+        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<>();
+        for (int i = 0; i < sensors.size(); i++) {
+            HashMap<String, Object> sensorTypeList = new HashMap<>();
+            sensorTypeList.put("Name", sensors.get(i).getName());
+            sensorTypeList.put("Value", sensors.get(i).getMaximumRange());
+            arrayList.add(sensorTypeList);
         }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+// создаем адаптер и устанавливаем тип адаптера - отображение двух полей
+        SimpleAdapter mHistory =
+                new SimpleAdapter(this, arrayList, android.R.layout.simple_list_item_2,
+                        new String[]{"Name", "Value"},
+                        new int[]{android.R.id.text1, android.R.id.text2});
+        listSensor.setAdapter(mHistory);
     }
 }
